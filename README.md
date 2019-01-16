ac0f1b527985a4c9f9d6744032dc4badcca34cda
DrakkoFire <ivan_palyaa99@hotmail.com>
2019-01-16 14:28:17 +0100

    Update estética
    Hay mayormente cambios estéticos y ligeros cambios en cuanto al funcionamiento como una nueva manera de declarar el tiempo entre cambios de configuración.

--- README.md
+++ README.md
@@ -6,12 +6,23 @@ Este repositorio es de las herramientas que voy creando, algunas son inútiles, 
 	- CMAC
 		[Se encarga de cambiar la configuración de red, tanto MAC como IP dentro de KALI LINUX]
 
-[+] INSTALACIÓN (Hay que tener Python 3)
+[+] REQUISITOS
+
+- MacChanger (Testeado con versión 1.7.0)
+- ip utility 
+
+[+] INSTALACIÓN (Hay que tener Python 3 testeado con 3.6.8)
 
 1) Ejecuta el comando "git clone https://github.com/DrakkoFire/DrakkoTools"
 2) Entra a la carpeta DrakkoTools y ejecuta ./init
 3) En caso de que "init" no sea ejecutable, pon el comando "chmod 744 init" en la carpeta donde está el archivo y podrás ejecutarlo con ./init
-4) Disfruta de odiseas ininterrumpibles de MAC e IP aleatorias (Cada 90 segundos cambia, se puede cambiar el valor de la variable "cambiame" al principio del script)
+4) Disfruta de odiseas ininterrumpibles de MAC e IP aleatorias
+
+[+] PARÁMETROS
+
+	-t [tiempo] -> El tiempo que tarda en volver a cambiar la configuración del script, por defecto son 90 segundos
+	-p -> Te dice las herramientas que necesita para funcionar
+	-h -> Ayuda
 
 [+] ACLARACIÓN
 
--- functions.py
+++ functions.py
@@ -3,12 +3,14 @@ import os,time,json,shlex,subprocess,argparse,random,sys, getopt
 from termcolor import colored
 
 def info():
-	print(colored("----------------------------------------------------------------","blue"))
-	print("Code made By: " + colored("DrakkoFire", "red"))
-	print("GitHub: " + colored("https://github.com/DrakkoFire", "red"))
-	print("Freelancer: " + colored("https://www.freelancer.com/u/DrakkoFire","red"))
-	print(colored("----------------------------------------------------------------","blue"))
-	print("(¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) (¬‿¬) \n")
+		# print('\033[1m' + '' + '\033[0m')
+	print(" " + '\033[1m' + colored("╔" + "═" * 75 + "╗" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("█" ,"blue") + " " * 75 + '\033[0m' + '\033[1m' +  colored("█" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("█" ,"blue") + '\033[1m' +  '{: ^84}'.format("Code made By: " + colored("DrakkoFire", "yellow")) + '\033[1m' + colored("█" ,"blue") )
+	print(" " + '\033[1m' + colored("█" ,"blue") + '\033[1m' +  '{: ^84}'.format("GitHub: " + colored("https://github.com/DrakkoFire", "yellow")) + '\033[1m' + colored("█" ,"blue") )
+	print(" " + '\033[1m' + colored("█" ,"blue") + '\033[1m' +  '{: ^84}'.format("Freelancer: " + colored("https://www.freelancer.com/u/DrakkoFire","yellow")) + '\033[1m' + colored("█" ,"blue") )
+	print(" " + '\033[1m' + colored("█" ,"blue") + '\033[0m' + " " * 75 + '\033[1m' + colored("█" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("╚" + "═" * 75 + "╝","blue") + '\033[0m')
 
 def clear():
 	os.system("clear")
@@ -27,27 +29,58 @@ def getIP(interf):
 
 	return data
 
+def displayInt():
+	nics = []
+	sub = subprocess.Popen(['/bin/sh', '-c', "ip -j token"], stdout=subprocess.PIPE)
+	stdout = sub.stdout.read()
+	json_data = json.loads(stdout)
+	print('{: ^80}'.format("Es aconsejable usar conexión por cable, la inalámbrica puede ser inestable"))
+	print(" " + '\033[1m' + colored("╔" + "═" * 31 + "╗" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("║" ,"blue") + '\033[1m' +  '{: ^31}'.format("Tarjetas de red") + '\033[1m' + colored("║" ,"blue") )
+	print(" " + '\033[1m' + colored("╠" + "═" * 5 + "╦" + "═" * 25 + "╣" ,"blue") + '\033[0m')
+	# print(" " + '\033[1m' + colored("╔" + "═" * 5 + "╦" + "═" * 25 + "╗" ,"blue") + '\033[0m')
+	for i in range(0,len(json_data)):
+		x = json_data[i]["ifname"]
+		nics.append(x)
+		print(" " + '\033[1m' + colored("║" ,"blue") + '\033[1m' + '{: ^5}'.format(i) + '\033[1m' + colored("║" ,"blue") + '\033[1m' + '{: ^25}'.format(x) + '\033[1m' + colored("║" ,"blue") )
+		if i + 1 == len(json_data):
+			print(" " + '\033[1m' + colored("╚" + "═" * 5 + "╩" + "═" * 25 + "╝" ,"blue") + '\033[0m')
+		else:
+			print(" " + '\033[1m' + colored("╠" + "═" * 5 + "╬" + "═" * 25 + "╣" ,"blue") + '\033[0m')
+		# print(" " + '\033[1m' + colored("║" ,"blue") + " " * 5 + x + " " * 10 + '\033[0m' + '\033[1m' +  colored("║" ,"blue") + '\033[0m')
+	return nics
+
+
 def getPieces(ip):
 	a = ip.split(".")
 	return a
 
-def selInterface():
+def selInterface(data):
 	a = True
 	while a:
 		try:
-		    b = input("Selecciona la interfaz [eth0]: ")
-		    if not b:
-		        b = "eth0"
-		        raise ValueError(colored("[+]","green")  + ' Interfaz ' +  colored(b,"green") + ' seleccionada')
+		    x = int(input(" Interfaz [0] ➮ "))
+		    if x >= len(data):
+		    	print('\033[1m' + colored("Error, la interfaz seleccionada no está en la lista", "red"))
 		    else:
-		    	print('Interfaz ' +  colored(b,"green") + ' seleccionada')
-		    	return b
+		    	print("Interfaz " + '\033[1m' + colored(data[x],"green") + '\033[0m' + " seleccionada")
 		    	a = False
-		except ValueError as e:
-		    print(e)
-		    return b
-		    a = False
+		    	return data[x]
+		    break
+		except ValueError:
+			print("Interfaz "  + colored(data[0], "green") + " seleccionada")
+			a = False
+			return data[0]
+		    # print(colored("[+]","green") + " Interfaz " + colored(b,"green"))
 
+def printData(interf,ip,mac,netmask):
+	print(" " + '\033[1m' + colored("╔" + "═" * 75 + "╗" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("║" ,"blue") + '\033[1m' +  '{: ^75}'.format("Configuración") + '\033[1m' + colored("║" ,"blue") )
+	print(" " + '\033[1m' + colored("╠" + "═" * 10 + "╦" + "═" * 22 + "╦" + "═"*20 + "╦" + "═"*20 + "╣" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("║" ,"blue") + '\033[1m' + '{: ^10}'.format("Interfaz") + '\033[1m' + colored("║" ,"blue") + '\033[1m' + '{: ^22}'.format("IP") + '\033[1m' + colored("║" ,"blue")+ '\033[1m' + '{: ^20}'.format("MAC") + '\033[1m' + colored("║" ,"blue")+ '\033[1m' + '{: ^20}'.format("Máscara") + '\033[1m' + colored("║" ,"blue") )
+	print(" " + '\033[1m' + colored("╠" + "═" * 10 + "╬" + "═" * 22 + "╬" + "═"*20 + "╬" + "═"*20 + "╣" ,"blue") + '\033[0m')
+	print(" " + '\033[1m' + colored("║" ,"blue") + '\033[1m' + '{: ^10}'.format(interf) + '\033[1m' + colored("║" ,"blue") + '\033[1m' + '{: ^22}'.format(ip) + '\033[1m' + colored("║" ,"blue")+ '\033[1m' + '{: ^20}'.format(mac) + '\033[1m' + colored("║" ,"blue")+ '\033[1m' + '{: ^20}'.format(netmask) + '\033[1m' + colored("║" ,"blue") )
+	print(" " + '\033[1m' + colored("╚" + "═" * 10 + "╩" + "═" * 22 + "╩" + "═"*20 + "╩" + "═"*20 + "╝" ,"blue") + '\033[0m')
 
 def checkIp(ip):
 	r = os.system("ping -c 2 %s > /dev/null 2>&1" % ip)
@@ -69,16 +102,16 @@ SEPARATORS = {
     'space': ' ',
 }
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Generate random MAC address')
-    parser.add_argument('-s', '--separator',
-                        choices=sorted(SEPARATORS.keys()), default='colon',
-                        help='What separator to use between the bytes in the output.')
-    return parser.parse_args()
+# def parse_args():
+#     parser = argparse.ArgumentParser(description='Generate random MAC address')
+#     parser.add_argument('-s', '--separator',
+#                         choices=sorted(SEPARATORS.keys()), default='colon',
+#                         help='What separator to use between the bytes in the output.')
+#     return parser.parse_args()
 
 def macGenerator():
-    args = parse_args()
-    separator = SEPARATORS[args.separator]
+    # args = parse_args()
+    separator = ":"
 
     mac = [ random.randint(0, 255) for x in range(0, 6) ]
     mac[0] = (mac[0] & 0xfc) | 0x02
@@ -110,24 +143,28 @@ def copia(archivo, revert = False):
 	backup = "/root/DrakkoTools/backup/copia-interfaces"
 	backup_dir = "/root/DrakkoTools/backup"
 	if revert == True:
-		print(colored("---[","blue") + "Moviendo " + colored(backup,"blue") + " -> " + colored(archivo,"green"))
+		print(colored("[ ]","blue") + "Moviendo " + colored(backup,"blue") + " -> " + colored(archivo,"green"), end="\r")
 		os.system("cp " + backup + " " + archivo)
-		print(colored("---[","red") + "Eliminando %s" % colored(backup,"red"))
+		print(colored("[+]","green"))
+		print(colored("[ ]","blue") + "Eliminando %s" % colored(backup,"red"), end="\r")
 		os.system("rm -R %s" % backup_dir)
+		print(colored("[+]","green"))
+		return True
 	else:
 		if os.path.exists(backup):
-			print(colored("---[","green") + "Existe")
-			print(colored("---[","blue") + "Restaurando copia de seguridad...")
+			print(colored("[+]","green") + "Existe")
+			print(colored("[ ]","blue") + "Restaurando copia de seguridad...", end="\r")
 			os.system("cp " + backup + " " + archivo)
-			print(colored("---[","green") + "Restaurado con éxito")
+			print(colored("[+]","green") + "Copias de seguridad restauradas con éxito")
 			return True
 		else:
-			print(colored("---[","red") + "No existe" + "\n" + colored("---[","blue") + "Realizando copia de seguridad...")
+			print(colored("[-]","red") + "No existe")
+			print(colored("[ ]","blue") + "Realizando copia de seguridad...", end="\r")
 			if not os.path.exists(backup_dir):
 				os.system("mkdir %s" % backup_dir + " && cp " + archivo + " " + backup)
 			else:
 				os.system("cp " + archivo + " " + backup)
-			print(colored("---[","green") + "Copia de seguridad hecha en " + colored(backup,"blue") )
+			print(colored("[+]","green") + "Copia de seguridad hecha con éxito \rRuta: " + colored(backup,"yellow") )
 			return True	
 
 def configurarRed(interf, ip,prefix,gw,mac):
@@ -146,34 +183,33 @@ def configurarRed(interf, ip,prefix,gw,mac):
 	print(colored("[+] MAC ","green") + "%s" % mac)
 	print("\n" + colored("---[","green") + "comprobando si hay copias de seguridad" + colored("]---","green"))
 	copia(archivo)
-	print(colored("---[","blue") + "Limpiando lista de direcciones...")
+	print(colored("[ ]","blue") + "Limpiando lista de direcciones...", end="\r")
 	os.system("ip addr flush dev %s" % interf)
 	os.system("ip route flush dev %s" % interf)
-	print(colored(	"---[","green") + "Lista limpiada con éxito")
+	print(colored("[+]","green") + "Lista de direcciones limpiada con éxito")
 	time.sleep(.5)
-	print(colored("---[","blue") + "Aplicando los cambios de red...")
+	print(colored("[ ]","blue") + "Aplicando los cambios de red...", end="\r")
 	os.system("echo \"%s\" >> " % conf + archivo)
 	time.sleep(1)
+	print(colored("[+]","green") + "Aplicando los cambios de red...")
 
 	try:
-		print(colored("---[","blue") + "Reiniciando servicios de red...")
+		print(colored("[ ]","blue") + "Reiniciando servicios de red..." , end="\r")
 		os.system("/etc/init.d/networking restart")
 	except:
-		print(("---[","red") + "Hubo un error")
+		print(("[-]","red") + "Hubo un error" + " " * 20)
 
-	#os.system("route add default gw %s" % gw)
-	print("\n\n" + colored("---[","yellow") + "Cambiando la MAC" + colored("]---","yellow") + "\n\n")
+	print(colored("[ ]","blue") + "Cambiando la MAC...", end="\r")
 	os.system("ifconfig %s down" % interf)
 	time.sleep(1)
 	os.system("macchanger -m {0} {1}".format(mac,interf))
 	time.sleep(1)
 	os.system("ifconfig %s up" % interf)
-	print("\n\n" + colored("---[","blue") + "configurando la puerta de enlace...")
+	print(colored("[+]","green") + "MAC cambiada con éxito")
+	print(colored("[ ]","blue") + "configurando la puerta de enlace...")
 	os.system("route add default gw %s" % gw)
+	print(colored("[+]","green") + "Puerta de enlace configurada correctamente")
 
 
 if __name__ == '__main__':
-    #configurarRed("eth0","192.168.1.18", 24,"192.168.1.1", macGenerator())
-    #info()
-    copia("/etc/network/interfaces", revert=True)
-    #getGw()
+	printData("eth0","192.168.1.8","b0:6e:bf:57:cf:46","255.255.255.0")
